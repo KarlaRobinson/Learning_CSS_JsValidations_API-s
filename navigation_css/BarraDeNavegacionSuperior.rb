92d4587fda607c0680b78a7017aabf18652e3306
codeamac03 <codeamac03@gmail.com>
2016-09-05 21:04:24 -0500

    reorganized files

---
+++ navigation_css/BarraDeNavegacionSuperior.rb
@@ -0,0 +1,33 @@
+.container {
+  margin: 0 auto;
+  width: 768px;
+}
+
+header {
+  background-color: black;
+}
+
+#logo {
+  display: block;
+  margin: 0 auto;
+}
+
+nav {
+  margin: 0 auto;
+  background-color: #9fa4a6;
+  height: 30px;
+  margin-top: -20px;
+}
+
+li {
+  display: inline;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+  font-size: 15px;
+  padding-left: 65px;
+  padding-right: 65px;
+  line-height: 32px;
+}
