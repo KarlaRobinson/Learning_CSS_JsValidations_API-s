c7fa975dc560cddfdb1fa27f2d9a18582aac8e0f
codeamac03 <codeamac03@gmail.com>
2016-09-06 14:31:04 -0500

    created deaf_grandma post method

--- deaf_grandma_skeleton/app/controllers/index.rb
+++ deaf_grandma_skeleton/app/controllers/index.rb
@@ -1,12 +1,17 @@
 get '/' do
   @abuelita = params[:abuelita]
-  
-  erb :index
-  # Visita app/views/index.erb
+  erb :index # Visita app/views/index.erb
 end
 
 
 post '/abuelita' do
   user_input = params[:user_input]
+  if user_input.include? ("!")
+    @message = "NO, NO DESDE 1983"
+  elsif user_input.downcase == "bye"
+    @message = "Adios!"
+  else
+    @message = "HUH?! NO TE ESCUCHO, HIJO!"
+  end
+  redirect to "/?abuelita=#{@message}"
 end
-
