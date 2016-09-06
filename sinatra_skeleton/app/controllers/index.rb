c24cc21931c1ed3b874c07f42e259ba42a961168
codeamac03 <codeamac03@gmail.com>
2016-09-05 20:18:43 -0500

    submit buttons sinatra

---
+++ sinatra_skeleton/app/controllers/index.rb
@@ -0,0 +1,27 @@
+get '/' do
+  puts "Este es un GET a (/)"
+  puts "Esto es lo que vale Params: #{params.inspect}"
+  erb :index # Esto renderea una vista llamada index.erb
+end
+
+get '/cool_url' do
+
+  @title = params[:title]
+  @price = params[:price]
+  @description = params[:description]
+
+  puts "Este es un GET a /cool_url"
+  puts "Esto es lo que vale Params: #{params.inspect}"
+  erb :get_cool_url  # Esto renderea una vista llamada get_cool_url.erb
+end
+
+post '/cool_url' do
+
+  @title = params[:title]
+  @price = params[:price]
+  @description = params[:description]
+
+  puts "Este es un POST a /cool_url"
+  puts "Esto es lo que vale Params: #{params.inspect}"
+  erb :post_cool_url  # Esto renderea una vista llamada post_cool_url.erb
+end
