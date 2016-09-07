c73f2708817c9f904eed099d8bc1ea24df3771cb
codeamac03 <codeamac03@gmail.com>
2016-09-07 11:04:34 -0500

    made nav bar become semi transparent when scrolling and created div around logo and menu options so that they are centered together as inline-block

--- fixed_navigation/navbar.js
+++ fixed_navigation/navbar.js
@@ -1,4 +1,10 @@
-$(window).on("scroll", function () {
-  // Aquí deberás escribir la lógica que modificará la barra
-  
+$(window).ready(function () {
+  $(window).scroll(function(){
+    console.log("inside");
+      var scroll_top = $(this).scrollTop();
+      //var scroll_top updates as we scroll to an integer representative of the height of the .container
+      if (scroll_top > 174){ $('.top-nav').css({ opacity: 0.5 });}
+      if (scroll_top < 174){ $('.top-nav').css({ opacity: 1.0 });}
+    $('#feedback').text('Currently at position: ' + scroll_top);
+  });
 });
