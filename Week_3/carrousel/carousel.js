5f0694a0b72bf95bb973562c04cebd077e7e1cf7
codeamac03 <codeamac03@gmail.com>
2016-09-30 18:45:14 -0500

    sliding photo carrusel

---
+++ Week_3/carrousel/carousel.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    /// go left
+  $('#previous_frame').on("click", function(){
+    var margin = parseInt($('.frames').css('marginLeft'));
+    var newmargin =  Math.ceil((margin + 1) / 620) * 620;
+
+    if (newmargin === 620){
+      $('.frames').css('marginLeft', '300%');
+      newmargin = -1240;
+    };
+
+    if (margin == 0){
+      $('.frames').css('marginLeft', '-300%');
+    };
+
+    $('.frames').animate({
+      marginLeft: newmargin,
+    }, 500 );
+      console.log(margin);
+      console.log(newmargin);
+  });
+
+
+    /// go right
+  $('#next_frame').on("click", function(){
+    var margin = parseInt($('.frames').css('marginLeft'));
+    var newmargin = Math.floor((margin - 1) / 620) * 620;
+
+    if (newmargin === -2480){
+      $('.frames').css('marginLeft', '0%');
+      newmargin = -620;
+    };
+
+    $('.frames').animate({
+      marginLeft: parseInt(newmargin),
+    }, 500 );
+      console.log(margin);
+      console.log(newmargin);
+  });
+});
