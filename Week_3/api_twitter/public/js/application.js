742e48d1519a957d97102e06e4e4f5bbfb1223cf
codeamac03 <codeamac03@gmail.com>
2016-10-06 19:17:26 -0500

    started adding ajax to make page white when loading

--- Week_3/api_twitter/public/js/application.js
+++ Week_3/api_twitter/public/js/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	// Este código corre después de que `document` fue cargado(loaded) 
-	// completamente. 
-	// Esto garantiza que si amarramos(bind) una función a un elemento 
-	// de HTML este exista ya en la página. 
+	$('.container').on("click", 'input[name=search]', function(event){
+    event.preventDefault();
+    $('#load').css('display', 'block');
+  });
 
 });
