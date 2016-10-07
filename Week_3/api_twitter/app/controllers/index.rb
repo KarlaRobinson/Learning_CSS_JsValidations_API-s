b6486112c76af10a3a5326ad6d8d1b2c21d694c7
codeamac03 <codeamac03@gmail.com>
2016-10-07 16:46:09 -0500

    added gif to appear when making a twitter request

--- Week_3/api_twitter/app/controllers/index.rb
+++ Week_3/api_twitter/app/controllers/index.rb
@@ -2,14 +2,8 @@ get '/' do
   erb :index
 end
 
-post '/fetch' do
-  username = params[:username]
-  redirect "/#{username}"
-end
-
-# get '/:handle' do
-#   @handle = CLIENT.user_timeline(params[:handle])
-#   erb :tweets
+# post '/fetch' do
+#   redirect "/#{params[:username]}"
 # end
 
 post '/tweet' do
@@ -17,26 +11,49 @@ post '/tweet' do
   redirect '/'
 end
 
-get '/:username' do
+post '/fetch' do
+  p "*" * 50
+  p params
   # finds user if they exist and if not a new user is created
-  @user = User.find_by_username(params[:username])
-  if @user == nil
-    @user = User.create(username: params[:username])
+  user = User.find_by_username(params[:username])
+  if user == nil
+    user = User.create(username: params[:username])
   end
-  # if user has no tweets, tweets are downloaded into db from twitter
-  "*"*50
-  last_tweet = CLIENT.user_timeline(@user.username).first.text
-  if @user.tweets.length == 0 || @user.tweets.where(text: last_tweet).length == 0
-    p "*"*50
-    p @user.tweets.where(text: last_tweet).length
 
-    CLIENT.user_timeline(@user.username).each do |tweet|
+  # checks if user has 0 tweets or if last db entry was more than an hour ago
+  last_db_entry = Time.now.utc - user.tweets.order(:created_at).last.created_at
+  if user.tweets.length == 0 || last_db_entry > 3600
+    return "twitter" #go to twitter to update db
+    # redirect "/twitter/#{user.username}"
+    # redirect "/test/#{user.username}"
+  else
+    return "db" #don't update db
+    # redirect "/db/#{user.username}"
+  end
+end
+
+get '/test/:username' do
+  erb :tweets
+end
+
+get '/db/:username' do
+  @user = User.find_by_username(params[:username])
+  erb :tweets
+end
+
+get '/twitter/:username' do
+  @user = User.find_by_username(params[:username])
+  # add new tweets into the db
+  CLIENT.user_timeline(@user.username).each do |tweet|
         t = Tweet.new(text: tweet.text, post_date: tweet.created_at)
         if t.save
           @user.tweets << t
         end
     end
-  end
-  @user
   erb :tweets
 end
+
+
+
+
+
--- Week_3/api_twitter/public/js/application.js
+++ Week_3/api_twitter/public/js/application.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
-	$('.container').on("click", 'input[name=search]', function(event){
-    event.preventDefault();
-    $('#load').css('display', 'block');
-  });
 
+  // $('.container').on("click", 'input[name=search]', function(event){
+  //   event.preventDefault();
+  //   $.post()
+  //   console.log("event: " + event);
+  //   // $('#load').css('display', 'block');
+  //   // username = $('input[name=username]').val();
+  //   // window.location.href = "/" + username;
+  // });
+
+  $('.container').on("click", 'input[name=search]', function(event){
+    event.preventDefault();
+    user = {}
+    user.username = $('input[name=username]').val();
+    window.location.href = "/test/" + user.username;
+    console.log("username " + user)
+    $.post('/fetch', user, function(go){
+      console.log("callback: " + go)
+      if(go === "twitter"){
+        $('#load').css('display', 'block');
+        $.get('/twitter/' + user.username, function(){
+          window.location.href = "/db/" + user.username;
+          // $('#load').css('display', 'none');
+        });
+      }else{
+        window.location.href = "/db/" + user.username;
+      }
+    });
+  });
 });
